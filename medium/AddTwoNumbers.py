250de082964a35ec75e1108f422c134d4de1cf3a
Vitor Valerio Hoffmann <137011464+vitor-hoffmann@users.noreply.github.com>
2025-01-24 10:27:14 -0300

    Update AddTwoNumbers.py

--- medium/AddTwoNumbers.py
+++ medium/AddTwoNumbers.py
@@ -10,7 +10,6 @@ class Solution:
         dummy = ListNode()
         current = dummy
         carry = 0
-
         while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
